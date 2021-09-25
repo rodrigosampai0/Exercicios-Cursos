a2c3e3d268cd86cc24c00c67bcef7b6a98b59bb6
Rodrigo Sampaio <16281210+r-sampaio@users.noreply.github.com>
2021-09-25 20:36:54 -0300

    update

---
+++ Aprenda Go 🇧🇷/Exercícios/Nível #1/01.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
+
+// - Esses são seus primeiros exercícios, e seus primeiros passos.
+// - Completando os exercícios dessa seção, você será um ninja nível 1.
+// - É o seu primeiro passo pra se tornar um developer ninja.
+// - Esses exercícios servem pra reforçar seu aprendizado. Só se aprende a programar programando. Ninguem aprende a andar de bicicleta assistindo vídeos de pessoas andando de bicicleta. É necessário botar a mão na massa.
+// - Eu vou começar explicando qual é o exercício. Aí vou pedir pra você dar pausa. Esse é o momento de por os miolos pra trabalhar, encontrar sua solução, tec-tec-tec, e rodar pra ver se funciona. Depois é só dar play novamente, ver a minha abordagem para a mesma questão, e comparar nossas soluções.
+// - Vamos lá:
+
+// - Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identificadores "x", "y", e "z".
+//     1. 42
+//     2. "James Bond"
+//     3. true
+// - Agora demonstre os valores nestas variáveis, com:
+//     1. Uma única declaração print
+//     2. Múltiplas declarações print
---
+++ Aprenda Go 🇧🇷/Exercícios/Nível #1/06.go
@@ -0,0 +1,3 @@
+// - Prova!
+// - Link: https://forms.gle/yutXCZCjsgBzMfxK8
+// - Se você deu pausa e fez todos os exercícios anteriores você mesmo, e só viu a resposta depois... e se você der pausa agora e fizer a prova inteira por conta própria, e só assistir as respostas depois... sabe o que isso quer dizer? Que você é ninja. Ninja nível 1. Tá no caminho certo pra ser um developer ninja mestre.
